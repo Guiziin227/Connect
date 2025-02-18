3019a0a74e958b7af90b526d2773c1f95fabe6b5
Guilherme Weber <gh63388@gmail.com>
2025-02-18 20:29:45 -0300

    Arrumando children props e estilizações

--- web-connect/src/app/page.tsx
+++ web-connect/src/app/page.tsx
@@ -1,11 +1,16 @@
-import { Button } from './../components/button'
+import { ArrowRight } from 'lucide-react'
+
+import { Button } from '@/components/Button'
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-2xl font-heading font-bold">Hello world!</h1>
-      <p>OIE</p>
-      <Button />
+
+      <Button>
+        Enviar
+        <ArrowRight />
+      </Button>
     </main>
   )
 }
--- web-connect/src/components/button.tsx
+++ web-connect/src/components/button.tsx
@@ -1,7 +1,17 @@
+import { ReactNode } from 'react'
+
 interface ButtonProps {
-  text?: string
+  children: ReactNode
 }
 
 export function Button(props: ButtonProps) {
-  return <button>{props.text || 'Enviar'}</button>
+  return (
+    <button
+      className=" flex justify-between items-center
+      px-5 h-12 bg-gray-500 text-blue font-semibold rounded-xl 
+      w-full cursor-pointer transition-colors duration-200 hover:bg-blue hover:text-gray-900"
+    >
+      {props.children || 'Enviar'}
+    </button>
+  )
 }
