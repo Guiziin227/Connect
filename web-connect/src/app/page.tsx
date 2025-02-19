47b45b6d3717d64b5c41e7db3738895e439bafde
Guilherme Weber <gh63388@gmail.com>
2025-02-19 17:37:47 -0300

    Fazendo o layout inicial

--- web-connect/src/app/page.tsx
+++ web-connect/src/app/page.tsx
@@ -1,31 +1,70 @@
-import { ArrowRight, Copy, Mail } from 'lucide-react'
+import Image from "next/image"
 
-import { Button } from '@/components/Button'
-import { IconButton } from './../components/Icon-Button'
-import { InputField, InputIcon, InputRoot } from '@/components/Input'
+import logo from "../assets/Logo.svg"
+import { ArrowRight, Mail, Radio, User } from "lucide-react"
+import { InputField, InputIcon, InputRoot } from "@/components/Input"
+import { Button } from "@/components/Button"
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-2xl font-heading font-bold">Hello world!</h1>
+    <main className="max-w-[1240px] mx-auto px-5 py-8 md:px-0">
+      <div className="min-h-dvh flex flex-col justify-center gap-16">
+        <div className="flex flex-col gap-8 items-center md:items-start">
+            <Image src={logo} alt="dev" width={108.5} height={30}/>
 
-      <Button>
-        Enviar
-        <ArrowRight />
-      </Button>
+            <h1 className="text-4xl text-center leading-none
+             font-heading font-medium flex flex-col md:text-7xl md:text-left"
+            >
+              <span className="text-blue">CodeCraft</span> 
+              Summit 2025
+            </h1>
+        </div>
 
-      <IconButton>
-        <Copy />
-      </IconButton>
+        <div className="flex gap-5 items-stretch flex-col md:flex-row">
+        <div className="flex-1 bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6">
+          <div className="flex items-center justify-between">
+            <h2 className="font-heading font-semibold text-gray-200 text-xl">Sobre o produto</h2>
+            <span className="text-purple font-semibold text-xs flex items-center gap-2">
+              <Radio className="size-5"/>
+              AO VIVO
+            </span>
+          </div>
+          <p className="text-gray-300 leading-relaxed text-sm md:text-base">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero reprehenderit ipsa, hic,
+             quod unde maiores deserunt suscipit amet cum minima enim!
+             <br/> 
+             <br/>
+             Et excepturi in consequatur optio.
+          </p>
+        </div>
 
-      <div>
-        <InputRoot>
-          <InputIcon>
-            <Mail />
-          </InputIcon>
 
-          <InputField placeholder="Digite seu email" />
-        </InputRoot>
+        <form className="bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[440px]">
+          <h2 className="font-heading font-semibold text-gray-200 text-xl">
+            Inscrição
+          </h2>
+
+          <div className="space-y-3">
+            <InputRoot>
+              <InputIcon>
+                <User/>
+              </InputIcon>
+              <InputField type="text" placeholder="Nome completo"/>
+            </InputRoot>
+
+            <InputRoot>
+              <InputIcon>
+                <Mail/>
+              </InputIcon>
+              <InputField type="email" placeholder="E-mail"/>
+            </InputRoot>
+          </div>
+
+          <Button type="submit">
+            Confirmar
+            <ArrowRight/>
+          </Button>
+        </form>
+      </div>
       </div>
     </main>
   )
